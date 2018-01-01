4c1a1edb7854e8109b5bd7395292471cff91cd1d
Gabriel Balta <programmer@Gabriels-MacBook-Air.local>
2018-01-01 13:42:30 +0000

    iPhone user interface complete

---
+++ Dev-profile/Dev-profile/View/CornerRadius.swift
@@ -0,0 +1,18 @@
+//
+//  CornerRadius.swift
+//  Dev-profile
+//
+//  Created by Gabriel Balta on 31/12/2017.
+//  Copyright © 2017 Gabriel Balta. All rights reserved.
+//
+
+import UIKit
+
+class CornerRadius: UIImageView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.cornerRadius = 10
+        layer.masksToBounds = true
+    }
+}
